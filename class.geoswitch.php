25c03c05127be3b9fc196db03447c1a87370adc8
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-12-17 16:57:59 -0500

    clear out all shortcodes aside from state

--- class.geoswitch.php
+++ class.geoswitch.php
@@ -39,14 +39,8 @@ class GeoSwitch {
         add_shortcode('geoswitch', array( 'GeoSwitch', 'switch_block' ));
         add_shortcode('geoswitch_case', array( 'GeoSwitch', 'switch_case' ));
 
-        add_shortcode('geoswitch_ip', array( 'GeoSwitch', 'get_ip' ));
-        add_shortcode('geoswitch_city', array( 'GeoSwitch', 'get_city' ));
         add_shortcode('geoswitch_state', array( 'GeoSwitch', 'get_state' ));
         add_shortcode('geoswitch_state_code', array( 'GeoSwitch', 'get_state_code' ));
-        add_shortcode('geoswitch_country', array( 'GeoSwitch', 'get_country' ));
-        add_shortcode('geoswitch_country_code', array( 'GeoSwitch', 'get_country_code' ));
-        add_shortcode('geoswitch_latitude', array( 'GeoSwitch', 'get_latitude' ));
-        add_shortcode('geoswitch_longitude', array( 'GeoSwitch', 'get_longitude' ));
     }
 
     public static function request_record($opts){
@@ -88,44 +82,21 @@ class GeoSwitch {
         $expandedContent = do_shortcode($content);
 
         if (is_null(self::$record)) {
-            if (!empty($atts['city']) ||
-                !empty($atts['state']) ||
-                !empty($atts['state_code']) ||
-                !empty($atts['country']) ||
-                !empty($atts['country_code']) ||
-                !empty($atts['within']) ||
-                !empty($atts['from'])) {
+
+            if (!empty($atts['state']) ||
+                !empty($atts['state_code'])) {
                     return '';
             }
             return '#'.strlen($expandedContent).'#'.$expandedContent;
         }
 
 
-        if ((empty($atts['city']) || strcasecmp($atts['city'], self::$record->city->name) == 0)
+        if ((empty($atts['state']) || strcasecmp($atts['state'], self::get_state($atts, $content)) == 0)
             &&
-            (empty($atts['state']) || strcasecmp($atts['state'], self::$record->mostSpecificSubdivision->name) == 0)
-            &&
-            (empty($atts['state_code']) || strcasecmp($atts['state_code'], self::$record->mostSpecificSubdivision->isoCode) == 0)
-            &&
-            (empty($atts['country']) || strcasecmp($atts['country'], self::$record->country->name) == 0)
-            &&
-            (empty($atts['country_code']) || strcasecmp($atts['country_code'], self::$record->country->isoCode) == 0)
-            &&
-            (empty($atts['within']) || self::within($atts['within'], $atts['from']))) {
+            (empty($atts['state_code']) || strcasecmp($atts['state_code'], self::get_state_code($atts, $content)) == 0)) {
             return '#'.strlen($expandedContent).'#'.$expandedContent;
         }
         return '';
-    }
-
-    public static function get_ip($atts, $content) {
-        return self::$user_ip;
-    }
-
-    public static function get_city($atts, $content) {
-        if (is_null(self::$record)) {
-            return '?';
-        }
-        return self::$record->city->name;
     }
 
     public static function get_state($atts, $content) {
@@ -142,35 +113,6 @@ class GeoSwitch {
         return self::$record->mostSpecificSubdivision->isoCode;
     }
 
-    public static function get_country($atts, $content) {
-        if (is_null(self::$record)) {
-            return '?';
-        }
-        return self::$record->country->name;
-    }
-
-    public static function get_country_code($atts, $content) {
-        if (is_null(self::$record)) {
-            return '?';
-        }
-        return self::$record->country->isoCode;
-    }
-
-    public static function get_latitude($atts, $content) {
-        if (is_null(self::$record)) {
-            return '?';
-        }
-        return self::$record->location->latitude;
-    }
-
-    public static function get_longitude($atts, $content) {
-        if (is_null(self::$record)) {
-            return '?';
-        }
-        return self::$record->location->longitude;
-    }
-
-
     public static function activation() {
         $default_options=array(
             'database_name'=>'GeoLite2-City.mmdb',
@@ -182,26 +124,6 @@ class GeoSwitch {
     public static function deactivation() {
         unregister_setting('geoswitch_options', 'geoswitch_options');
         delete_option('geoswitch_options');
-    }
-
-    private static function within($within, $from) {
-        $within = 0.0 + $within;
-        $from = explode(',', $from, 2);
-
-        $pi80 = M_PI / 180;
-        $lat1 = ($from[0] + 0.0) * $pi80;
-        $lng1 = ($from[1] + 0.0) * $pi80;
-        $lat2 = self::$record->location->latitude * $pi80;
-        $lng2 = self::$record->location->longitude * $pi80;
-
-        $r = 6372.797; // mean radius of Earth in km
-        $dlat = $lat2 - $lat1;
-        $dlng = $lng2 - $lng1;
-        $a = sin($dlat / 2) * sin($dlat / 2) + cos($lat1) * cos($lat2) * sin($dlng / 2) * sin($dlng / 2);
-        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
-        $km = $r * $c;
-
-        return self::$useKm ? ($km <= $within) : (($km * 0.621371192) <= $within);
     }
 
     private static function get_user_ip() {
